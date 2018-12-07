b1512ef70ee540d7d4a5e10beb5db2ad182f9a95
liyong.liu <liyong.liu@yitu-inc.com>
2018-12-07 16:34:50 +0800

    add feature: writeDirectly method

--- src/main/java/util/logging/LogWriter.java
+++ src/main/java/util/logging/LogWriter.java
@@ -7,14 +7,50 @@ import java.util.Date;
 
 public class LogWriter {
     private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-    private static final DateFormat timeFormat = new SimpleDateFormat("HH:mm:ss");
-    public static void writeLog(String message) throws IOException {
+    private static final DateFormat timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+    private static final String fs = System.getProperties().getProperty("file.separator");
+
+    private static void write(String fileName, String message, Exception exp, boolean addDate) {
+        File logFile = new File(fileName);
+        File logDir = new File("logs");
+        try {
+            if(!logDir.isDirectory()){
+                logDir.mkdir();
+            }
+            if (!logFile.exists()) {
+                logFile.createNewFile();
+            }
+            BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(logFile, true)));
+            if(addDate){
+                writer.write(timeFormat.format(new Date()) + ": " + message);
+            }else {
+                writer.write(message);
+            }
+            writer.newLine();
+            if (exp != null) {
+                PrintWriter pw = new PrintWriter(writer);
+                exp.printStackTrace(pw);
+            }
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void recordLog(String prefix, String message, Exception e) {
         String nowDate = dateFormat.format(new Date());
-        String logPath = "log_" + nowDate + ".log";
-        File logFile = new File(logPath);
-        if (!logFile.exists())
-            logFile.createNewFile();
-        BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(logFile)));
-        writer.write(timeFormat.format(new Date())+": "+message);
+        String nowTime = timeFormat.format(new Date());
+        String Log = String.format("%s====> %s", nowTime, message);
+        if (e != null) {
+            System.err.println(Log);
+        }else {
+            System.out.println(Log);
+        }
+        LogWriter.write("logs" + fs + prefix + "_" + nowDate + ".log", message, e, true);
+    }
+
+    public static void writeDirectly(String fileName, String message){
+        String nowDate = dateFormat.format(new Date());
+        write("logs" + fs + fileName + "_" + nowDate + ".log",message,null,false);
     }
 }
