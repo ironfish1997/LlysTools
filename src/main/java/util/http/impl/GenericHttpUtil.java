c93989edc74b7a38f0978d668c1e2b157904d7bb
liyong.liu <liyong.liu@yitu-inc.com>
2018-12-07 16:27:30 +0800

    overload

--- src/main/java/util/http/impl/GenericHttpUtil.java
+++ src/main/java/util/http/impl/GenericHttpUtil.java
@@ -1,5 +1,6 @@
 package util.http.impl;
 
+import util.http.HttpUtil;
 import com.google.api.client.http.*;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.http.json.JsonHttpContent;
@@ -7,16 +8,12 @@ import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.FieldInfo;
 import com.google.api.client.util.Lists;
-import util.http.HttpUtil;
-import util.logging.LogWriter;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -28,59 +25,31 @@ public class GenericHttpUtil implements HttpUtil {
     private static final HttpRequestFactory REQUEST_FACTORY = new NetHttpTransport().createRequestFactory();
     private static final JsonFactory JSON_FACTORY = new JacksonFactory();
 
-    public GenericHttpUtil() {
+    protected GenericHttpUtil(){
+
     }
 
     public static String doGet(String url, Map<String, Object> headerFields, Map<String, Object> params, Integer timeout) throws IOException {
         try {
             url = getParamsUrl(url, params);
             HttpRequest request = REQUEST_FACTORY.buildGetRequest(new GenericUrl(url));
-            HttpHeaders headers = new HttpHeaders();
-            //请求头中设置页面编码为utf-8
-            headers.setContentEncoding("utf-8");
-            setRequestHeaders(headerFields, request, headers);
-            if (timeout > 0) {
-                request.setConnectTimeout(timeout);
-                request.setReadTimeout(timeout);
-            }
-            HttpResponse response = request.execute();
-            if (response.getStatusCode() != 200) {
-                return response.getStatusMessage();
-            }
-            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getContent(), StandardCharsets.UTF_8));
-            StringBuilder result = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
-            return result.toString();
+            setRequestHeaders(headerFields, request, timeout);
+            return getResponseResultString(request);
         } catch (IOException e) {
-            recordLog(e);
+            e.printStackTrace();
             throw e;
         }
     }
+
 
     public static String doPost(String url, Map<String, Object> headerFields, Map<String, Object> bodyParams, Integer timeout) throws IOException {
         try {
             HttpContent content = new JsonHttpContent(JSON_FACTORY, bodyParams);
             HttpRequest request = REQUEST_FACTORY.buildPostRequest(new GenericUrl(url), content);
-            HttpHeaders headers = new HttpHeaders();
-            //请求头中设置页面编码为utf-8
-            headers.setContentEncoding("utf-8");
-            setRequestHeaders(headerFields, request, headers);
-            if (timeout > 0) {
-                request.setConnectTimeout(timeout);
-                request.setReadTimeout(timeout);
-            }
-            BufferedReader reader = new BufferedReader(new InputStreamReader(request.execute().getContent(), StandardCharsets.UTF_8));
-            StringBuilder result = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
-            return result.toString();
+            setRequestHeaders(headerFields, request, timeout);
+            return getResponseResultString(request);
         } catch (IOException e) {
-            recordLog(e);
+            e.printStackTrace();
             throw e;
         }
     }
@@ -89,24 +58,10 @@ public class GenericHttpUtil implements HttpUtil {
         try {
             HttpContent content = new JsonHttpContent(JSON_FACTORY, bodyParams);
             HttpRequest request = REQUEST_FACTORY.buildPutRequest(new GenericUrl(url), content);
-            HttpHeaders headers = new HttpHeaders();
-            //请求头中设置页面编码为utf-8
-            headers.setContentEncoding("utf-8");
-            headers.setContentType("application/json");
-            setRequestHeaders(headerFields, request, headers);
-            if (timeout > 0) {
-                request.setConnectTimeout(timeout);
-                request.setReadTimeout(timeout);
-            }
-            BufferedReader reader = new BufferedReader(new InputStreamReader(request.execute().getContent(), StandardCharsets.UTF_8));
-            StringBuilder result = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
-            return result.toString();
+            setRequestHeaders(headerFields, request, timeout);
+            return getResponseResultString(request);
         } catch (IOException e) {
-            recordLog(e);
+            e.printStackTrace();
             throw e;
         }
     }
@@ -116,21 +71,13 @@ public class GenericHttpUtil implements HttpUtil {
             if (url == null) throw new NullPointerException("URL cannot be null");
             url = getParamsUrl(url, params);
             HttpRequest request = REQUEST_FACTORY.buildHeadRequest(new GenericUrl(url));
-            HttpHeaders headers = new HttpHeaders();
-            //请求头中设置页面编码为utf-8
-            headers.setContentEncoding("utf-8");
-            headers.setContentType("application/json");
-            setRequestHeaders(headerFields, request, headers);
-            if (timeout > 0) {
-                request.setConnectTimeout(timeout);
-                request.setReadTimeout(timeout);
-            }
+            setRequestHeaders(headerFields, request, timeout);
             HttpResponse response = request.execute();
             if (response.getStatusCode() != 200)
                 throw new IOException(response.getStatusMessage());
             return response.getHeaders();
         } catch (Exception e) {
-            recordLog(e);
+            e.printStackTrace();
             throw e;
         }
     }
@@ -139,24 +86,26 @@ public class GenericHttpUtil implements HttpUtil {
         try {
             url = getParamsUrl(url, params);
             HttpRequest request = REQUEST_FACTORY.buildDeleteRequest(new GenericUrl(url));
-            if (timeout > 0) {
-                request.setConnectTimeout(timeout);
-                request.setReadTimeout(timeout);
-            }
-            HttpResponse response = request.execute();
-            if (response.getStatusCode() != 200)
-                throw new IOException(response.getStatusMessage());
-            BufferedReader reader = new BufferedReader(new InputStreamReader(response.getContent(), StandardCharsets.UTF_8));
-            StringBuilder result = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                result.append(line);
-            }
-            return result.toString();
+            setRequestHeaders(null, request, timeout);
+            return getResponseResultString(request);
         } catch (IOException e) {
-            recordLog(e);
+            e.printStackTrace();
             throw e;
         }
+    }
+
+    private static String getResponseResultString(HttpRequest request) throws IOException {
+        HttpResponse response = request.execute();
+        if (response.getStatusCode() != 200) {
+            return response.getStatusMessage();
+        }
+        BufferedReader reader = new BufferedReader(new InputStreamReader(response.getContent(), StandardCharsets.UTF_8));
+        StringBuilder result = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+        return result.toString();
     }
 
     private static String getParamsUrl(String url, Map<String, Object> params) throws UnsupportedEncodingException {
@@ -166,9 +115,9 @@ public class GenericHttpUtil implements HttpUtil {
             StringBuilder urlBuilder = new StringBuilder(url);
             while (inter.hasNext()) {
                 Map.Entry entry = (Map.Entry) inter.next();
-                String key = java.net.URLEncoder.encode((String) entry.getKey(),
+                String key = java.net.URLEncoder.encode(entry.getKey()+"",
                         "utf-8");
-                String val = java.net.URLEncoder.encode((String) entry.getValue(),
+                String val = java.net.URLEncoder.encode( entry.getValue()+"",
                         "utf-8");
                 urlBuilder.append(key).append("=").append(val).append("&");
             }
@@ -178,14 +127,14 @@ public class GenericHttpUtil implements HttpUtil {
         return url;
     }
 
-    private static void recordLog(Exception e) throws IOException {
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");//设置日期格式
-        String errLog = String.format("%s: DoHeadError==>%s", df.format(new Date()), e.getMessage());
-        System.err.println(errLog);
-        LogWriter.writeLog(errLog);
-    }
-
-    private static void setRequestHeaders(Map<String, Object> headerFields, HttpRequest request, HttpHeaders headers) {
+    private static void setRequestHeaders(Map<String, Object> headerFields, HttpRequest request, Integer timeout) {
+        HttpHeaders headers = new HttpHeaders();
+        //请求头中设置页面编码为utf-8
+        headers.setContentEncoding("utf-8");
+        if (timeout > 0) {
+            request.setConnectTimeout(timeout);
+            request.setReadTimeout(timeout);
+        }
         if (headerFields != null && headerFields.size() != 0) {
             for (Object o : headerFields.entrySet()) {
                 Map.Entry entry = (Map.Entry) o;
